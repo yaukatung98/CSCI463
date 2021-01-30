e8361e4207c83a9a6e52a78037de3773ef1370dd
Ynot <60165407+yaukatung98@users.noreply.github.com>
2021-01-30 21:44:31 +0800

    Update MISP-UPDATE.sh

--- MISP-UPDATE.sh
+++ MISP-UPDATE.sh
@@ -4,18 +4,10 @@
 
 # Check if running with sudo in a bash script
 
-if [[ "$EUID" = 0 ]]; then
-    echo "(1) already root"
-else
-    sudo -k # make sure to ask for password on next sudo
-    if sudo true; then
-        echo "(2) correct password"
-    else
-        echo "(3) wrong password"
-        exit 1
-    fi
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (sudo -i)"
+  exit
 fi
-# Do your sudo stuff here. Password will not be asked again due to caching.
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
