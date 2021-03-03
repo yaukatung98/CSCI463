5719def5404f40da4e520ce48777bcac77d4106f
Ynot <60165407+yaukatung98@users.noreply.github.com>
2021-03-03 12:16:28 +0800

    add

---
+++ MISP/misp-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# One Liner Update Script
+# bash <(curl -s -L https://raw.githubusercontent.com/yaukatung98/CSCI463/master/ssh-install.sh)
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# UPDATE THE SERVER
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# INSTALL MISP 
+
+wget -O /tmp/INSTALL.sh https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh
+
+echo -ne '                       [0%] Installing MISP and all the dependencies\r'
+bash /tmp/INSTALL.sh -A
+echo -ne '                       [100%] Done\r'
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
